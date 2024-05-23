dd8a22cd527712e8577148c9ab0de3062dc626c5
Александр <alex.tunin1993@yandex.ru>
2024-05-23 16:45:20 +0300

    исправление разметки

--- src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
@@ -2,25 +2,24 @@ import {
   CurrencyIcon,
   Counter,
 } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./ingredient-card.module.css";
 
 export default function IngridientCard({ item }) {
   return (
     <>
-      <div>
-        <Counter count={1} />
-        <div>
-          <img src={item.image} alt={item.name} />
-          <div>
-            <p className="text text_type_main-default pr-1 m-1 p-1">
-              {item.price}
-              <CurrencyIcon type="primary" />
-            </p>
-          </div>
-          <div>
-            <p className="text text_type_main-default pr-1">{item.name}</p>
-          </div>
-        </div>
-      </div>
+                <div className={styles.item} key={item._id}>
+                  <Counter count={item.count} />
+                  <picture className={styles.picture}>
+                    <img className={styles.image} src={item.image} alt={item.name} />
+                    <span className={styles.price}>
+                        {item.price}
+                        <CurrencyIcon type="primary" />
+                    </span>
+                      <p className={styles.text}>
+                        {item.name}
+                      </p>
+                  </picture>
+                </div>
     </>
   );
 }
--- src/components/burger-ingredients/ingredients-menu/ingredients-menu.jsx
+++ src/components/burger-ingredients/ingredients-menu/ingredients-menu.jsx
@@ -1,91 +1,39 @@
 import IngredientCard from "../ingredient-card/ingredient-card";
 import data from "../../../data/data.json";
-import {
-  CurrencyIcon,
-  Counter,
-} from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./ingredients-menu.module.css";
 
 export default function IngredientsMenu() {
   return (
     <>
-        <div className="">
+        <div className={styles.tab}>
           <h2 className={styles.title}>Булки</h2>
           <div className={styles.items}>
             {data
               .filter((item) => item.type === "bun")
               .map((bun) => (
-                <div className={styles.item} key={bun._id}>
-                  <Counter count={1} />
-                  <div>
-                    <img src={bun.image} alt={bun.name} />
-                    <div>
-                      <p className={styles.price}>
-                        {bun.price}
-                        <CurrencyIcon type="primary" />
-                      </p>
-                    </div>
-                    <div>
-                      <p className={styles.text}>
-                        {bun.name}
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                <IngredientCard item={bun} key={bun._id} count={0} />                
               ))}
           </div>
         </div>
 
-        <div>
+        <div className={styles.tab}>
           <h2 className={styles.title}>Соусы</h2>
           <div className={styles.items}>
             {data
               .filter((item) => item.type === "sauce")
               .map((sauce) => (
-                <div className={styles.item} key={sauce._id}>
-                  <Counter count={1} />
-                  <div>
-                    <img src={sauce.image} alt={sauce.name} />
-                    <div>
-                      <p className={styles.price}>
-                        {sauce.price}
-                        <CurrencyIcon type="primary" />
-                      </p>
-                    </div>
-                    <div>
-                      <p className={styles.text}>
-                        {sauce.name}
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                <IngredientCard item={sauce} key={sauce._id} count={0} /> 
               ))}
           </div>
         </div>
 
-        <div>
+        <div className={styles.tab}>
           <h2 className={styles.title}>Начинки</h2>
           <div className={styles.items}>
             {data
               .filter((item) => item.type === "main")
               .map((main) => (
-                <div className={styles.item} key={main._id}>
-                  <Counter count={1} />
-                  <div>
-                    <img src={main.image} alt={main.name} />
-                    <div>
-                      <p className={styles.price}>
-                        {main.price}
-                        <CurrencyIcon type="primary" />
-                      </p>
-                    </div>
-                    <div>
-                      <p className={styles.text}>
-                        {main.name}
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                <IngredientCard item={main} key={main._id} count={0} /> 
               ))}
           </div>
         </div>
