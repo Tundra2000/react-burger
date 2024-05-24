75b12441b34afedc19332cff9372674dc1f254b0
Александр <alex.tunin1993@yandex.ru>
2024-05-24 13:10:59 +0300

    модальное окно

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -5,14 +5,24 @@ import {
   DragIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burger-constructor.module.css";
+import Modal from '../modal/modal';
+import React from 'react';
 
 export default function BurgerConstructor(props) {
-
+  const [visible, setVisible] = React.useState(false);
 
   const bun = props.data.find((item) => item.type === "bun");
   const sauce = props.data.find((item) => item.type === "sauce");
   const main = props.data.find((item) => item.type === "main");
   let price = 1000;//bun.price * 2 + sauce.price + main.price;
+
+  const  openModal = (data) => {
+    setVisible(true);
+  }
+  
+  const closeModal = () => {
+    setVisible(false);
+  }
 
   return (
     <>
@@ -86,12 +96,22 @@ export default function BurgerConstructor(props) {
           <p className={styles.price}>
             {price + " "}
             <CurrencyIcon type="primary" />
-            <Button htmlType="button" type="primary" size="medium">
+            <Button htmlType="button" type="primary" size="medium" onClick={openModal}>
               Оформить заказ
             </Button>
           </p>
         </div>
       </div>
+      {
+        visible && 
+          (
+            <Modal header="Внимание!" onClose={closeModal}> 
+              
+              <h1>Спасибо за внимание!</h1>
+              
+            </Modal> 
+          ) 
+      }
     </>
   );
 }
